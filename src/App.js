866a8bdbfd4486af997119f349873a2ca756a5f3
Jo Cohen <jojocohen57@gmail.com>
2024-12-16 13:34:13 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './index.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import { useHistory, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
